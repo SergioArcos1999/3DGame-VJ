c2f97e47b8d9175a78ff8c38883fae51bcc12a94
sergioarcos1999 <sergi.arcos@ielesvinyes.net>
2020-11-16 18:15:39 +0100

    MainMenu + basic Game physics & camera

---
+++ 3DGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame ()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Loads the next scene
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!"); //We use this so we can see if we close the app. Because in Unity we can't see it.
+        Application.Quit();
+    }
+}
