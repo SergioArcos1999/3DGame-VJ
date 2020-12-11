22c881e22596ec838374f9a12044ffa9f85c39d4
Vmorais22 <victormoraisllahi@gmail.com>
2020-12-11 09:37:42 +0100

    bug fixes

---
+++ 3DGame/Assets/Scripts/blockPinchosMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockPinchosMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = Time.deltaTime;
+        transform.Rotate(0.0f, 40.0f * delta, 0.0f);
+    }
+}
