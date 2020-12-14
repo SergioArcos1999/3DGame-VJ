d9f0c61a7bfaafc76584e7ad8f679bc30af34fd9
SergioArcos1999 <sergi.arcos@ielesvinyes.net>
2020-12-14 22:43:17 +0100

    Coin animation + ilumination

--- 3DGame/Assets/Scripts/CoinBehaviour.cs
+++ 3DGame/Assets/Scripts/CoinBehaviour.cs
@@ -8,6 +8,7 @@ public class CoinBehaviour : MonoBehaviour
     private float finalTime;
     private float destroyTime;
     private bool touch = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +19,30 @@ public class CoinBehaviour : MonoBehaviour
     void Update()
     {
         float delta = Time.deltaTime;
+        
         transform.Rotate(0.0f, 40.0f * delta, 0.0f);
-        finalTime = 80f;
+        
+        
+        finalTime = 65f;
         destroyTime += 1;
-        if (destroyTime  >= finalTime && touch == true) {
-            gameObject.SetActive(false);
-            Destroy(gameObject);
+        if (destroyTime*delta  <= finalTime*delta && touch == true) {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.03f, transform.position.z);
+            transform.Rotate(0.0f, 80.0f * delta*7, 0.0f);
+            if(destroyTime == finalTime) Destroy(gameObject);
         }
+        
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && !touch)
         {
+            
             destroyTime = 0f;
             audioSource.Play();
+            
+            
+            
             touch = true;
         }
     }
